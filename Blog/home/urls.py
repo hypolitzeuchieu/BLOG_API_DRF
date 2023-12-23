514b5d53a621e4823240ea221ac9f8bd8eec6ed8
Hypolit <hypolitzeuchieu@gmail.com>
2023-12-23 15:41:42 +0100

    creation public classe

--- Blog/home/urls.py
+++ Blog/home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import BlogView
+from .views import BlogView, PublicView
 
 
 urlpatterns = [
     path('blog', BlogView.as_view()),
+    path('display', PublicView.as_view()),
+
 ]
--- Blog/home/views.py
+++ Blog/home/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from .models import Blog
 from .serializers import BlogSerializers
 from django.db.models import Q
+from django.core.paginator import Paginator
+
 
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import serializers
@@ -14,26 +16,6 @@ class BlogView(APIView):
 
     permission_classes = [IsAuthenticated]
     authentication_classes = [JWTAuthentication]
-
-    def get(self, request):
-        try:
-            blogs = Blog.objects.filter(user=request.user)
-            if request.GET.get('search'):
-                search = request.GET.get('search')
-                blogs = blogs.filter(Q(title__icontains=search) | Q(blog_text__icontains=search))
-
-            serializer = BlogSerializers(blogs, many=True)
-
-            return Response({
-                'data': serializer.data,
-                'message': 'blogs fetched successfully'
-            }, status=status.HTTP_201_CREATED)
-        except Exception as e:
-            print(e)
-            Response({
-                'data': {},
-                'message': 'invalid credentials'
-            }, status=status.HTTP_400_BAD_REQUEST)
 
     def post(self, request):
 
@@ -106,7 +88,6 @@ class BlogView(APIView):
 
             serializer = BlogSerializers(data=data)
 
-
             if not blogs.exists():
                 return Response({
                     'data': {},
@@ -130,4 +111,31 @@ class BlogView(APIView):
             return Response({
                 'data': {},
                 'message': 'invalid credentials'
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PublicView(APIView):
+
+    def get(self, request):
+        try:
+            blogs = Blog.objects.all().order_by('?')
+
+            if request.GET.get('search'):
+                search = request.GET.get('search')
+                blogs = blogs.filter(Q(title__icontains=search) | Q(blog_text__icontains=search))
+
+            page_number = request.GET.get('page', 1)
+            paginator = Paginator(blogs, 6)
+
+            serializer = BlogSerializers(paginator.page(page_number), many=True)
+
+            return Response({
+                'data': serializer.data,
+                'message': 'blogs fetched successfully'
+            }, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            print(e)
+            Response({
+                'data': {},
+                'message': 'invalid credentials  or invalid page'
             }, status=status.HTTP_400_BAD_REQUEST)
