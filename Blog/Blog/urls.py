338a33cf74faf331d2e621ede64ced822aa42165
Hypolit <hypolitzeuchieu@gmail.com>
2023-12-23 12:35:50 +0100

    creation of login view by jwt

--- Blog/Blog/urls.py
+++ Blog/Blog/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
+    path('home/', include('home.urls')),
+
 ]
