aef30c93e3e58b40411b24edd3915eace98eb824
Hypolit <hypolitzeuchieu@gmail.com>
2023-12-23 15:14:21 +0100

    creation of patch method

--- Blog/home/views.py
+++ Blog/home/views.py
@@ -58,3 +58,42 @@ class BlogView(APIView):
                 'data': {},
                 'message': 'invalid credentials'
             }, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request):
+        try:
+            data = request.data
+            data['user'] = request.user.id
+            blogs = Blog.objects.filter(uid=data.get('uid'))
+
+            if not blogs.exists():
+                return Response({
+                    'data': {},
+                    'message': 'invalid blog uid'
+                }, status=status.HTTP_400_BAD_REQUEST)
+
+            if request.user != blogs[0].user:
+                return Response({
+                    'data': {},
+                    'message': ' you are not authorized to this'
+                   }, status=status.HTTP_400_BAD_REQUEST)
+
+            serializer = BlogSerializers(blogs[0], data=data, partial=True)
+
+            if not serializer.is_valid():
+                return Response({
+                    'data': serializer.errors,
+                    'message': 'something went wrong'
+                    }, status=status.HTTP_400_BAD_REQUEST)
+
+            serializer.save()
+            return Response({
+                'data': serializer.data,
+                'message': 'blog updated successfully'
+            }, status=status.HTTP_201_CREATED)
+
+        except Exception as e:
+            print(e)
+            return Response({
+                'data': {},
+                'message': 'invalid credentials'
+            }, status=status.HTTP_400_BAD_REQUEST)
